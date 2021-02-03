05f18274b623d8a19d977a2c1e5baf864c5c5243
Tife-code <soderubolu@gmail.com>
2021-02-03 15:33:22 +0100

    Here's my solution to the SCA test assessment

--- src/app.py
+++ src/app.py
@@ -3,7 +3,19 @@ import os
 
 
 def palindrome(s):
-    # your code goes here
+    global check
+    check =[]
+    for char in s:
+        check.append(char)
+        if char == " ":
+            check.remove(" ")
+    palindrome=check[::-1]
+    if palindrome == check:
+        return True
+    else:
+        return False
+   
+    
 
 def solution(s):
     return palindrome(s)
@@ -14,3 +26,5 @@ if __name__ == "__main__":
         sys.exit(os.error("Argment required"))
 
     print(solution(sys.argv[1]))
+
+
